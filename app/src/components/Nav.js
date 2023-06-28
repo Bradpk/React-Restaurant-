6de0a3e34c880268b62d795a9eff11984cfbae1a
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-28 15:06:02 -0400

    g

---
+++ app/src/components/Nav.js
@@ -0,0 +1,13 @@
+function Navbar(){
+    return       <nav>
+    <div className='nav'>
+        <ol>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Hours</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ol>
+    </div>
+</nav>
+}
