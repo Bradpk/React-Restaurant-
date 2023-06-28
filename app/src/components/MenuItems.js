e8b3749a3da7530bb5582ed3762751e953f89b84
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-28 19:56:13 -0400

    g

---
+++ app/src/components/MenuItems.js
@@ -0,0 +1,15 @@
+function MenuItems() {
+    return (
+        <div className='menuItems'>
+
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Card Title</h5>
+                    <p className="card-text">Card content</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItems
