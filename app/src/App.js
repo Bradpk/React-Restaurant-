9175b187c3646b5820fad67bbca76219c8bddec0
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-28 16:25:08 -0400

    g

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 // I did this 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
