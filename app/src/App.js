56f175edb1f0f04cb6915b42374f0f4c11c103b4
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-28 14:48:42 -0400

    g

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+// I did this 
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
