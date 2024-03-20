ce65829a5b01190ad0b7700ee75f57b29212248e
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-03-20 14:41:11 -0400

    g

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
 import Navbar from './components/Nav';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
         <Navbar />
         <MainContent />
         <Footer />
-    </div>
+        <div className='content'>
+          {/* <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+        </Switch> */}
+      </div>
+      </div>
+    </Router>
   );
 }
 
